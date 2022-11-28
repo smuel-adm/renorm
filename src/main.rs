a806605c9ec0b11e50fc7da707b6992454900641
Stefan Müller <stefan.mueller@hitachiastemo.com>
2022-11-28 10:14:45 +0100

    Parse args

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use std::env;
+
+
+struct CSV {
+    old_filename: String,
+    new_filename: String,
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    // PARAM 1 CSV
+    let args: Vec<String> = env::args().collect();
+    //dbg!(args);
+    let csv_path = &args[1];
+
+    print!("{}", csv_path);
+
+    // parse csv
+
+    // construct Vec of csv's
+
+    // loop throug vec
+        // rename files
+    
 }
