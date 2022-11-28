0f8db85db68d1d15ae98fae11730ece1121a02d4
Stefan Müller <stefan.mueller@hitachiastemo.com>
2022-11-28 10:28:28 +0100

    Parse CSV

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,16 @@
 use std::env;
+use csv::ReaderBuilder;
+use serde::Deserialize;
 
-
+#[derive(Debug, Deserialize)]
 struct CSV {
+    // 9123917,45756807,de,DIN EN ISO 4759-1,200104,PDF
     old_filename: String,
+    value1: String,
+    lang: String,
     new_filename: String,
+    value2: String,
+    extension: String,
 }
 
 
@@ -16,10 +23,14 @@ fn main() {
     print!("{}", csv_path);
 
     // parse csv
+    let mut rdr = ReaderBuilder::new()
+        .has_headers(false)
+        .from_path(&csv_path).unwrap();
+    for result in rdr.deserialize() {
+        // loop throug vec
+            // rename files
+        let line: CSV = result.unwrap();
+        println!("{:?}", line);
+    }
 
-    // construct Vec of csv's
-
-    // loop throug vec
-        // rename files
-    
 }
